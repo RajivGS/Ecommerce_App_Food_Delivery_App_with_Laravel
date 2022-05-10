bf008102b61e6e6d73eb73109fdda5e669349967
RajivGS <recall5220@gmail.com>
2022-05-10 16:29:26 +0545

    fifth_commit

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,5 +1,6 @@
 import 'package:foodies_palpa/data/repository/popular_product_repo.dart';
 import 'package:foodies_palpa/models/product_model.dart';
+import 'package:foodies_palpa/utils/colors.dart';
 import 'package:get/get.dart';
 
 class PopularProductController extends GetxController {
@@ -20,5 +21,41 @@ class PopularProductController extends GetxController {
       _isLoaded = true;
       update();
     } else {}
+  }
+
+//setup a method responsible for increasing or decreasing items based on button click
+  int _quantity = 0;
+  int get quantity => _quantity;
+  int _inCartItem = 0;
+  int get inCartItem => _inCartItem + _quantity;
+
+  void setQuantity(bool isIncrement) {
+    if (isIncrement) {
+      _quantity = checkQuantity(_quantity + 1);
+    } else {
+      _quantity = checkQuantity(_quantity - 1);
+    }
+    update();
+  }
+
+  int checkQuantity(int quantity) {
+    if (quantity < 0) {
+      Get.snackbar("Item count", "You can't reduce more !",
+          backgroundColor: AppColors.mainColor);
+      return 0;
+    } else if (quantity > 20) {
+      Get.snackbar("Item count", "You can't add more !",
+          backgroundColor: AppColors.mainColor);
+      return 20;
+    } else {
+      return quantity;
+    }
+  }
+
+  void initProduct() {
+    _quantity = 0;
+    _inCartItem = 0;
+    //if exist
+    // get from Storage _inCartItem
   }
 }
